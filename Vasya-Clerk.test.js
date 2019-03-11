85cdda3ceecf864ba93d40b04ea30072bcc28109
Tim Johnson <pavementii@gmail.com>
2019-03-11 11:49:00 -0400

    [testing] clean up for test

--- Vasya-Clerk.test.js
+++ Vasya-Clerk.test.js
@@ -1,15 +1,11 @@
-import {haveExactChange} from './Vasya-Clerk'
+const tickets = require('./Vasya-Clerk')
 
-describe('haveExactChange', () => {
+describe('tickets', () => {
   // mockup of item objects
 
   test('Should return true if all customers have exact change', () => {
-    var peopleInLine = [25]
-    expect(haveExactChange(peopleInLine)).toEqual(true)
-  })
-  test('Should return 0 if there are no transactions', () => {
-    var itemObjs = []
-    expect(ReturnAuthVerify.getTransactionsTotal(itemObjs)).toEqual(0)
+    var peopleInLine = [50]
+
+    expect(tickets(peopleInLine)).toEqual(true)
   })
 })
-
