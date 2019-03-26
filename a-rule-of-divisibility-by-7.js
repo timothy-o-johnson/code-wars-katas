1a588df70f915acd6c3f79796f3614405e0d6fc1
Tim Johnson <pavementii@gmail.com>
2019-03-26 11:31:55 -0400

    [sx] committing under github user name

--- a-rule-of-divisibility-by-7.js
+++ a-rule-of-divisibility-by-7.js
@@ -1,4 +1,5 @@
 /* Instructions
+
 Output
 A number m of the form 10x + y is divisible by 7 if and only if x − 2y is divisible by 7. In other words, subtract twice the last digit from the number formed by the remaining digits. Continue to do this until a number known to be divisible or not by 7 is obtained; you can stop when this number has at most 2 digits because you are supposed to know if a number of at most 2 digits is divisible by 7 or not.
 
